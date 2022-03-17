bba29eb31fff9afa82566e1525bde0fdd3350bd1
salybu <rkdmsdl93@gmail.com>
2022-03-17 21:18:35 +0900

    feat: create counter 0 tag & test case

--- react-test-app/src/App.test.js
+++ react-test-app/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("the counter starts at 0", () => {
+  render(<App />); // App component 를 렌더링
+  const counterElement = screen.getByTestId("counter");
+  // ID 가 'counter' 인 counterElement 상수를 생성함. screen object 를 이용해서 원하는 element 에 접근 (접근할 때 ID 로)
+  expect(counterElement).toHaveTextContent(0);
+  // counter Element 시작하는 값이 0 이어야 함. ID 가 counter 인 엘레먼트의 텍스트가 0 인지 테스트
 });
