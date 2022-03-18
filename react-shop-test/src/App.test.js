1a4cf5c5d949ffafe02f595d38b873582145f431
salybu <rkdmsdl93@gmail.com>
2022-03-18 18:20:09 +0900

    chore: ESLint setting

--- react-shop-test/src/App.test.js
+++ react-shop-test/src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+  const lintTest = screen.getByRole("button", {
+    name: "lintTest",
+  });
+
+  // expect(lintTest.textContent).toBe('lintTest') // 이 라인에 빨간 줄 에러 뜨는 것으로 Lint 가 적용되었는지 확인, quickfix 하면 아래에 있는 코드로 변경됨
+  expect(lintTest).toHaveTextContent("lintTest");
 });
